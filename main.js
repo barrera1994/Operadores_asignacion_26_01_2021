31467615eea6ad522d59a0d6a596290e35004535
andres julian barrera diaz <ing.julian.26@gmail.com>
2021-01-26 18:59:30 -0500

    Operadores_asignacion_26_01_2021

---
+++ main.js
@@ -0,0 +1,48 @@
+//Operadores de asignación
+let x = 10;
+let y = 20;
+
+x=y;
+console.log("asignacion x=y :"+ x);
+
+//adicion
+x = 10;
+y = 20;
+
+x+=y;
+console.log("adicion x+=y :"+ x);
+
+//sustracción
+x = 10;
+y = 20;
+
+x-=y;
+console.log("sustracción x-=y :"+ x);
+
+//multiplicacion
+x = 10;
+y = 20;
+
+x*=y;
+console.log("multiplicación x*=y :"+ x);
+
+//division
+x = 10;
+y = 20;
+
+x/=y;
+console.log("division x/=y :"+ x);
+
+//resto
+x = 10;
+y = 20;
+
+x%=y;
+console.log("resto x%=y :"+ x);
+
+//exponenciacion
+x = 10;
+y = 20;
+
+x**=y;
+console.log("exponenciación x**=y :"+ x);
