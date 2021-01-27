428e98dfefa11d2cb5b6ec5588cb57ea3030fddb
andres julian barrera diaz <ing.julian.26@gmail.com>
2021-01-26 20:57:55 -0500

    Operadores_aritmeticos_26_01_2021

--- main.js
+++ main.js
@@ -1,48 +1,34 @@
-//Operadores de asignación
+//Operadores de concatenacion
+let altura = 1.80;
+let edad = 27;
+let nombre="andres barrera";
+
+console.log("primera manera de concatenar (+) "+nombre+edad);
+console.log("segunda manera de concatenar ('')"+""+altura+edad);
+let con=(nombre.concat(altura));
+console.log("tercera manera de concatenar (concat)"+con);
+console.log("cuarta manera de concatenar (,)",altura,edad);
+console.log("quinta manera de concatenar (`${}`)");
+console.log(`${nombre} ${altura}`);
+
+//operador de decremento
 let x = 10;
-let y = 20;
+console.log(`decremento (--) ${x--} valor inicial ${x}`);
 
-x=y;
-console.log("asignacion x=y :"+ x);
+ x = 10;
+console.log(`division (/) ${x/2} valor inicial ${x} `);
 
-//adicion
-x = 10;
-y = 20;
+ x = 10;
+console.log(`exponencial (**) ${x**2} valor inicial ${x} `);
 
-x+=y;
-console.log("adicion x+=y :"+ x);
+ x = 10;
+console.log(`incremento (++) ${x++} valor inicial ${x} `);
 
-//sustracción
-x = 10;
-y = 20;
+ x = 10;
+console.log(`multiplicacion (*) ${x*2} valor inicial ${x} `);
 
-x-=y;
-console.log("sustracción x-=y :"+ x);
+ x = 10;
+console.log(`residuo de una division (%) ${x%2} valor inicial ${x} `);
 
-//multiplicacion
-x = 10;
-y = 20;
-
-x*=y;
-console.log("multiplicación x*=y :"+ x);
-
-//division
-x = 10;
-y = 20;
-
-x/=y;
-console.log("division x/=y :"+ x);
-
-//resto
-x = 10;
-y = 20;
-
-x%=y;
-console.log("resto x%=y :"+ x);
-
-//exponenciacion
-x = 10;
-y = 20;
-
-x**=y;
-console.log("exponenciación x**=y :"+ x);
+x = -10;
+console.log(`unary (-) ${-x} valor inicial ${x} `);
